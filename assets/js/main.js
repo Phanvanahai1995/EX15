acfbbfe717fef4aed4275a685e06dede1817f685
phanvanhai <phanvanhaixdddk49@gmail.com>
2024-02-23 20:52:05 +0700

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,13 @@ const actionProfile = document.querySelector(".header-action__profile");
 const headerAction = document.querySelector(".header-action");
 const overlayAction = document.querySelector(".header-action__overlay");
 const action = document.querySelector(".header-action__profile-active");
+const profileInfo = document.querySelector(".profile-info");
 const bar = document.querySelector(".fa-bars ");
 const user = document.querySelector(".fa-user");
 const navbar = document.querySelector(".navbar");
 
 action.addEventListener("click", function () {
-  headerAction.classList.add("header-action__hide");
+  profileInfo.classList.add("profile-info__hide");
   overlayAction.classList.add("header-action__overlay-hide");
 });
 
@@ -18,6 +19,7 @@ bar.addEventListener("click", function () {
 
 overlayAction.addEventListener("click", function () {
   headerAction.classList.remove("header-action__hide");
+  profileInfo.classList.remove("profile-info__hide");
   overlayAction.classList.remove("header-action__overlay-hide");
   navbar.classList.remove("navbar-hide");
 });
